c0e3f3fc80a252cd7de6b4be1068fb45eb93bdc0
Aman Singh <50237942+Aman1440@users.noreply.github.com>
2020-06-12 16:01:40 +0530

    Update Shuffle.py

--- Shuffle.py
+++ Shuffle.py
@@ -1,1 +1,10 @@
 #program to shuffle a deck of cards
+import itertools, random
+
+deck = list(itertools.product(range(1,14), ["spade","heart","Diamond","club"]))
+
+random.shuffle(deck)
+
+print("your combination of cards is :")
+for i in range(5):
+  print(deck[i][0],"of", deck[i][1])
